165aa3366be9d8fc92470a6d74a90330acfc6cc6
Amine Kadoum <ma_kadoum@esi.dz>
2025-12-05 18:17:48 +0100

    updating react to 19.2.1 and next to 16.0.7

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import createAnalyzer from "@next/bundle-analyzer"
+
+const withBundledAnalyzer = createAnalyzer({
+  enabled : process.env.ANALYZER === 'true'
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withBundledAnalyzer(nextConfig);
